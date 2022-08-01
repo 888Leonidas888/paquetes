d7aafa554d20b7649dae68f48b01d316ed91f071
Jhony <jhonny14_1@hotmail.com>
2022-08-01 11:56:54 -0500

    Se termina el curso de Go desde 0.

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
+//para utilizar nuestro paquete 'greet' y el de terceros
+//debemos crear un módulo,con el siguiente comando en terminal
+//go mod init [github.com/888Leonidas888/paquetes] , lo que esta entre llaves
+//la ruta de nuestro repositorio hacia nuestro paquete
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/888Leonidas888/paquetes/greet"
+
+	"rsc.io/quote/v3"
+)
 
 func main() {
-	fmt.Println(greet.Spanish())
+	//Usando nuestro propio paquete
+	fmt.Println("Hello")
+	fmt.Println("Saludo en inglés", greet.English())
+	fmt.Println("Saludo en italiano", greet.Italian())
+	fmt.Println("Saludo en español", greet.Spanish())
+
+	//Usando paquete de terceros
+	//para usar el paquete de un tercero debo hacerlo con el comando en terminal
+	//go get [rsc.io/quote]
+	fmt.Println("saludo desde un paquete de tercero", quote.HelloV3())
+	fmt.Println("Versión mayor", quote.Concurrency())
 }
